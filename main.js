b9ee6a62d7d9db834cf0a27284ca645db0cc21de
alextaylor <alextaylor515@gmail.com>
2022-06-09 09:44:41 -0400

    commit tree 1a0aa654f6b6920d3d5a979a9caeaea5d95cbefd

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const like = document.querySelectorAll(".like-glyph")
 
+like.forEach(ele=> {
+  ele.addEventListener("pointerdown",(e)=>{
+    if (e.target.textContent === EMPTY_HEART) {
+      mimicServerCall().then(clickSuccessEmpty).catch(clickFailed)
+      e.target.textContent = FULL_HEART
+      e.target.classList.add("activated-heart")
+    }
+    else {
+      mimicServerCall().then(clickSuccessFull).catch(clickFailed) 
+      e.target.textContent = EMPTY_HEART
+      e.target.classList.remove("activated-heart")
+    }
 
+  })
+})
+function clickSuccessEmpty() {
 
+  console.log(like)
+}
+function clickSuccessFull(){
+  e = EMPTY_HEART
+}
+function clickFailed(data){
+  const modal= document.getElementById("modal")
+  modal.classList.remove("hidden")
+  modal.textContent = data
+  setTimeout(()=> modal.classList.add("hidden"),3000)
+
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
